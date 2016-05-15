88553cbc365c7c3c7678b6675cdcdd374eeb609a
jtandrews <jtandrew@uc.cl>
2016-05-15 05:05:34 -0300

    Functional version of API

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   
   root 'welcome#hello'
 
-  post 'instagram/tag/buscar#buscarTag'
+  post 'instagram/tag/buscar' => 'instagram#buscarTag'
+
+  get 'test' => 'instagram#prueba'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
